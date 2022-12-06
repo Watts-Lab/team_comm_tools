ddc22c0b475cffbafaa55cee7df31e292f4d6e1d
Xinlan Emily Hu <xehu@stanford.edu>
2022-12-06 18:42:50 -0500

    Complete Gini Coefficient (with the exception of integration testing). Also, Close #41.

--- feature_engine/featurize.py
+++ feature_engine/featurize.py
@@ -15,24 +15,63 @@ the features/ folder.
 
 # import our feature files
 from features.basic_features import *
-from features.emotion_features import *
+#from features.emotion_features import *
+from features.gini_coefficient import *
 
+'''
+@param df = the name of the dataframe on which the operation is being applied.
+	- assumes that the df is a chat-by-chat setup in which each row is 1 chat.
+	- assumes that the chat is stored in a column called 'message'
+@param feature_name = the name of the column you want the feature to be named
+@param function_name = the name of the function used to create the feature
+'''
+def create_chat_level_feature(df, feature_name, function_name):
+	df[feature_name] = df['message'].apply(lambda x: function_name(str(x)))
+	return(df)
+
+
+# Main Function
 if __name__ == "__main__":
 
 	# import the data from the data file
 	INPUT_FILE_PATH = './data/raw_data/jury_conversations_with_outcome_var.csv'
-	OUTPUT_FILE_PATH = './output/jury_output.csv'
+	OUTPUT_FILE_PATH_CHAT_LEVEL = './output/jury_output_chat_level.csv'
 
 	conversation_data = pd.read_csv(INPUT_FILE_PATH)
-	output_data = conversation_data
+	output_data_chats = conversation_data
 
-	# call features here
-	# single chat-level features are applied per *message*
-	# in the future, we can also generate per-conversation features -- but may need to format the input data differently
-	output_data['num_words'] = output_data['message'].apply(lambda x: count_words(str(x)))
-	output_data['num_chars'] = output_data['message'].apply(lambda x: count_characters(str(x)))
-	output_data['emotions'] = output_data['message'].apply(lambda x: get_emotions(str(x)))
 
+	### CHAT-LEVEL FEATURES --------------------------------------------------------------
+	'''
+	chat-level features take place on the single-utterance level. Each person will make
+	many utterances throughout a conversation.
+	'''
+	output_data_chats = create_chat_level_feature(output_data_chats, "num_words", count_words)
+	output_data_chats = create_chat_level_feature(output_data_chats, "num_chars", count_characters)
+
+	# TODO -- when generated, these emotion labels don't make sense. Need new ones.
+	# output_data_chats = create_chat_level_feature(output_data_chats, "num_chars", count_characters)
+	# generate output file
+	output_data_chats.to_csv(OUTPUT_FILE_PATH_CHAT_LEVEL)
+
+
+
+	### CONVERSATION-LEVEL FEATURES --------------------------------------------------------------
+	OUTPUT_FILE_PATH_CONVERSATION_LEVEL = './output/jury_output_conversation_level.csv'
+
+	'''
+	conversation-level features take place at the level of the entire conversation; 
+	we may need to pass the chat-level features into additional functions in order 
+	to calculate conversation-level features. As an example, Gini is a conversational-
+	level summary of how many chats each person in the conversation made.
+	'''
+	# basic output data on the conversation level -- just the batch and round nums
+	output_data_conversations = conversation_data.groupby(['batch_num', 'round_num']).sum(numeric_only = True).reset_index().iloc[: , :2]
+	
+	# generate all conversation level features here
+	output_data_conversations = pd.merge(output_data_conversations, get_gini(output_data_chats, "num_words"), on=['batch_num', 'round_num'])
+	output_data_conversations = pd.merge(output_data_conversations, get_gini(output_data_chats, "num_chars"), on=['batch_num', 'round_num'])
 
 	# generate output file
-	output_data.to_csv(OUTPUT_FILE_PATH)
+	output_data_conversations.to_csv(OUTPUT_FILE_PATH_CONVERSATION_LEVEL)
+
