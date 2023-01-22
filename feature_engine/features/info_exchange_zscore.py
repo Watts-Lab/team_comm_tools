2652e0a0f7a32041d9026316f26b9cefefdf35fd
Xinlan Emily Hu <xehu@stanford.edu>
2023-01-22 02:38:59 -0500

    minor fixes to information exchange + comment on the z-score at the conversation level. We should take a second look at this before resolving Issue #46.

--- feature_engine/features/info_exchange_zscore.py
+++ feature_engine/features/info_exchange_zscore.py
@@ -3,7 +3,7 @@ import pandas as pd
 import scipy.stats as stats
 
 # source: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.zscore.html
-
+from features.basic_features import *
 
 # Get the wordcount used to calculate z-scores: total word count minus first_singular pronouns
 def info_exchange_wordcount(text):
@@ -22,6 +22,11 @@ def get_zscore_chats(chats_data, on_column):
   chats_data['zscore_chats'] = stats.zscore(chats_data[on_column])
   return (chats_data)
 
+
+# TODO - I imagined that z-score is a chat-level feature (it's the z-score of the chat, relative to other chats)? 
+# I think this function should still calculate a separate z-score for each chat, but simply locally cluster the calculations
+# at the within-conversation level.
+# Does this feature make sense at the conversation level? We should revisit the paper to look at their method: https://www.cs.cmu.edu/~ylataus/files/TausczikPennebaker2013.pdf
 
 # Get the z-score of each conversation
 '''
