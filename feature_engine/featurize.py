a7bef6a1092cd2bf89334a033c8e235cb5f07b05
Xinlan Emily Hu <xehu@stanford.edu>
2023-02-26 23:07:25 -0500

    changes related to creating a full analysis pipeline / exploring how to create said pipeline

--- feature_engine/featurize.py
+++ feature_engine/featurize.py
@@ -11,28 +11,12 @@ from feature_builder import FeatureBuilder
 # Main Function
 if __name__ == "__main__":
 	# Instantiating the Feature Generating Class
-	# feature_builder = FeatureBuilder(
-	# 	input_file_path = "../feature_engine/data/raw_data/juries_tiny_for_testing.csv",
-	# 	output_file_path_chat_level = "../feature_engine/output/jury_TINY_output_chat_level.csv",
-	# 	output_file_path_conv_level = "../feature_engine/output/jury_TINY_output_conversation_level.csv"
-	# )
+	feature_builder = FeatureBuilder(
+		input_file_path = "../feature_engine/data/raw_data/juries_tiny_for_testing.csv",
+		output_file_path_chat_level = "../feature_engine/output/jury_TINY_output_chat_level.csv",
+		output_file_path_conv_level = "../feature_engine/output/jury_TINY_output_conversation_level.csv"
+	)
 	# Calling the "engine"/"driver" function of the FeatureBuilder class 
 	# that creates the features, and writes them in output.
 	
-	# feature_builder.featurize(col="message")
-
-
-	### TODO --- Temp by Emily to run the data twice (once on juries, once on CSOP)
-	# juries_feature_builder = FeatureBuilder(
-	# 	input_file_path = "../feature_engine/data/raw_data/jury_conversations_with_outcome_var.csv",
-	# 	output_file_path_chat_level = "../feature_engine/output/jury_output_chat_level.csv",
-	# 	output_file_path_conv_level = "../feature_engine/output/jury_output_conversation_level.csv"
-	# )
-	# juries_feature_builder.featurize(col="message")
-
-	csop_feature_builder = FeatureBuilder(
-		input_file_path = "../feature_engine/data/raw_data/csop_conversations.csv",
-		output_file_path_chat_level = "../feature_engine/output/csop_output_chat_level.csv",
-		output_file_path_conv_level = "../feature_engine/output/csop_output_conversation_level.csv"
-	)
-	csop_feature_builder.featurize(col="message")
+	feature_builder.featurize(col="message")
