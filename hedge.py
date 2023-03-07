739924c84c5c361a6c255b6064ae2f0c3274e271
PriyaDCosta <76833529+PriyaDCosta@users.noreply.github.com>
2023-03-07 13:10:52 -0500

    Updated the hedge
    Updated hedge as per the first method. For the second method, yet to finalize the code after discussion with the team

--- hedge.py
+++ hedge.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #returns the total number of hedge words present in the data set (We can add more hedge words)
-def hedge(df,on_column):
+def hedge1(df,on_column):
     hedge_words = ["sort of", "kind of", "I guess", "I think", "a little", "maybe", "possibly", "probably"]
     return  df[on_column].str.count("|".join(hedge_words)).sum()
 
@@ -10,4 +10,4 @@ data = {
     "name": ["I am sort of","I guess I am crazy"]
 }
 df = pd.DataFrame(data)
-hedge(df,"name")
+hedge1(df,"name")
