8351c8713c8baa79b73a36412837e96e906c906d
Candice (Yuluan) Cao <95985540+Candicecao8@users.noreply.github.com>
2023-03-16 23:20:15 -0400

    Update calculate_conversation_level_features.py
    Updated feature extractions related to word_mimicry.py

--- feature_engine/utils/calculate_conversation_level_features.py
+++ feature_engine/utils/calculate_conversation_level_features.py
@@ -128,14 +128,14 @@ class ConversationLevelFeaturesCalculator:
 	# Number of questions
 	self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_average(self.chat_data, 'Qnum', 'average_Qnum_chats'),
+            right=get_average(self.chat_data, 'Qnum', 'average_Qnum'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
 
         self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_stdev(self.chat_data, 'Qnum', 'std_Qnum_chats'),
+            right=get_stdev(self.chat_data, 'Qnum', 'std_Qnum'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
@@ -143,14 +143,14 @@ class ConversationLevelFeaturesCalculator:
 	# Proportion of clarification questions
 	self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_average(self.chat_data, 'NTRI', 'average_NTRI_chats'),
+            right=get_average(self.chat_data, 'NTRI', 'average_NTRI'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
 
         self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_stdev(self.chat_data, 'NTRI', 'std_NTRI_chats'),
+            right=get_stdev(self.chat_data, 'NTRI', 'std_NTRI'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
@@ -158,14 +158,14 @@ class ConversationLevelFeaturesCalculator:
 	# Word type-to-token ratio
 	self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_average(self.chat_data, 'word_TTR', 'average_word_TTR_chats'),
+            right=get_average(self.chat_data, 'word_TTR', 'average_word_TTR'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
 
         self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_stdev(self.chat_data, 'NTRI', 'std_word_TTR_chats'),
+            right=get_stdev(self.chat_data, 'NTRI', 'std_word_TTR'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
@@ -174,18 +174,47 @@ class ConversationLevelFeaturesCalculator:
 	# Proportion of first person pronouns
 	self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_average(self.chat_data, 'first_pronouns_proportion', 'average_first_pronouns_proportion_chats'),
+            right=get_average(self.chat_data, 'first_pronouns_proportion', 'average_first_pronouns_proportion'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
 
         self.conv_data = pd.merge(
             left=self.conv_data,
-            right=get_stdev(self.chat_data, 'first_pronouns_proportion', 'std_first_pronouns_proportion_chats'),
+            right=get_stdev(self.chat_data, 'first_pronouns_proportion', 'std_first_pronouns_proportion'),
             on=['batch_num', 'round_num'],
             how="inner"
         )
 	
+	# Function word mimicry
+	self.conv_data = pd.merge(
+            left=self.conv_data,
+            right=get_average(self.chat_data, 'FuncWordAcc', 'average_FuncWordAcc'),
+            on=['batch_num', 'round_num'],
+            how="inner"
+        )
+
+        self.conv_data = pd.merge(
+            left=self.conv_data,
+            right=get_stdev(self.chat_data, 'FuncWordAcc', 'std_FuncWordAcc'),
+            on=['batch_num', 'round_num'],
+            how="inner"
+        )
+	
+	# Content word mimicry
+	self.conv_data = pd.merge(
+            left=self.conv_data,
+            right=get_average(self.chat_data, 'ContWordAcc', 'average_ContWordAcc'),
+            on=['batch_num', 'round_num'],
+            how="inner"
+        )
+
+        self.conv_data = pd.merge(
+            left=self.conv_data,
+            right=get_stdev(self.chat_data, 'ContWordAcc', 'std_ContWordAcc'),
+            on=['batch_num', 'round_num'],
+            how="inner"
+        )
 	
 
     def get_talkative_member_features(self) -> None:
