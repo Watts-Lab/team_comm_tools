da42536a049e8a2f5576c6c7e56c4f3c05942900
Priya Ronald DCosta <76833529+PriyaDCosta@users.noreply.github.com>
2023-03-21 17:04:43 -0400

    removed test files

--- feature_engine/features/entropy.py
+++ feature_engine/features/entropy.py
@@ -59,15 +59,3 @@ def create_sets(filepath):
         set = set(file.read().split())
     return set       
      
-        
-#Test for entropy
-data = {
-    "name": ["you are a bad person","You are a happy, joyful, lovely person"]
-}
-
-df = pd.DataFrame(data)
-
-set1 = ["happy", "joyful", "lovely"]
-set2 = ["bad"]
-
-print(ngram_dialog_act_entropy(df,'name',1,set1,set2,'positive','negative'))
