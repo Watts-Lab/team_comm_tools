e8424a352467d4bdf2c6790e3755b937702856b7
Xinlan Emily Hu <xehu@stanford.edu>
2023-03-21 23:36:44 -0400

    make count_syllables much simpler

--- feature_engine/features/readability.py
+++ feature_engine/features/readability.py
@@ -5,20 +5,7 @@ import nltk
 # Define the function to calculate the Dale-Chall score
 def count_syllables(word):
     # count the number of syllables in a word
-    count = 0
-    vowels = "aeiouy"
-    word = word.lower().strip(".:;?!")
-    if word[0] in vowels:
-        count += 1
-    for index in range(1, len(word)):
-        if word[index] in vowels and word[index-1] not in vowels:
-            count += 1
-    if word.endswith("e"):
-        count -= 1
-    if count == 0:
-        count += 1
-    return count
-
+    return(sum(list(map(word.lower().count, "aeiou"))))
 
 def count_words(text):
     # count the number of words in a text
