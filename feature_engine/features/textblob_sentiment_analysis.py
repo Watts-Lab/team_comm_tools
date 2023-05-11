657cf91d2c0f35db3ab8c346883ad281df4df6a1
Priya DCosta <76833529+PriyaDCosta@users.noreply.github.com>
2023-05-11 11:39:01 -0400

    Sentiment Analysis using TextBlob

---
+++ feature_engine/features/textblob_sentiment_analysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from textblob import TextBlob
+import statistics as stat
+
+'''
+Polarity:Ranges between -1 to 1,  -1 is most negative, 0 is neutral, 1 is positive
+Subjectivity: Ranges between 0 to 1, 0 - purely factual, 1 - pure opinion
+'''
+def get_subjectivity_score(string):
+    return TextBlob(string).sentiment.subjectivity
+def get_polarity_score(string):
+    return TextBlob(string).sentiment.polarity
