0da4d689949e2a7ed9256afe8daf732ae8c912d0
Xinlan Emily Hu <xehu@stanford.edu>
2023-05-11 11:38:29 -0400

    completed hedge1 feature

--- feature_engine/features/hedge.py
+++ feature_engine/features/hedge.py
@@ -2,7 +2,5 @@ import pandas as pd
 import re
 
 #returns the total number of hedge words present in the data set (We can add more hedge words)
-def is_hedged_sentence_1(df,on_column):
-
-    pattern = "|".join(hedge_words)
-    return df[on_column].apply(lambda x: int(bool(re.search(pattern, x, re.IGNORECASE))))
+def is_hedged_sentence_1(num_hedge_words):
+    return 1 if num_hedge_words > 0 else 0
