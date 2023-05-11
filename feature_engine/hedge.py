8dbb18c0a7c1eb8b51ed8bcf22fbd31781b9b58e
Priya DCosta <76833529+PriyaDCosta@users.noreply.github.com>
2023-05-11 11:21:48 -0400

    hedge for merge

---
+++ feature_engine/hedge.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import re
+
+#returns the total number of hedge words present in the data set (We can add more hedge words)
+def is_hedged_sentence_1(df,on_column):
+    # TODO - hedge_words is not defined.
+    pattern = "|".join(hedge_words)
+    return df[on_column].apply(lambda x: int(bool(re.search(pattern, x, re.IGNORECASE))))
