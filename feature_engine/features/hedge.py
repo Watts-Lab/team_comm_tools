15c3648269e402b0a2e2a9495056be85dd55845c
Priya DCosta <76833529+PriyaDCosta@users.noreply.github.com>
2023-05-11 11:22:39 -0400

    Update hedge.py

--- feature_engine/features/hedge.py
+++ feature_engine/features/hedge.py
@@ -1,13 +1,8 @@
 import pandas as pd
+import re
 
 #returns the total number of hedge words present in the data set (We can add more hedge words)
-def hedge(df,on_column):
-    hedge_words = ["sort of", "kind of", "I guess", "I think", "a little", "maybe", "possibly", "probably"]
-    return  df[on_column].str.count("|".join(hedge_words)).sum()
+def is_hedged_sentence_1(df,on_column):
 
-#Test Data
-data = {
-    "name": ["I am sort of","I guess I am crazy"]
-}
-df = pd.DataFrame(data)
-hedge(df,"name")
+    pattern = "|".join(hedge_words)
+    return df[on_column].apply(lambda x: int(bool(re.search(pattern, x, re.IGNORECASE))))
