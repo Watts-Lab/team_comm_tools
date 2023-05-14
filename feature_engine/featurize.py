91d3a6fec793f2df31a62b89cfc6c6e0d02683fc
Xinlan Emily Hu <xehu@stanford.edu>
2023-05-14 18:11:49 -0400

    emily adding test cases

--- feature_engine/featurize.py
+++ feature_engine/featurize.py
@@ -64,13 +64,13 @@ if __name__ == "__main__":
 	# csop_feature_builder.featurize(col="message")
 
 	# CSOP II (Nak Won Rim)
-	# csopII_feature_builder = FeatureBuilder(
-	# 	input_file_path = "../feature_engine/data/raw_data/csopII_conversations_withblanks.csv",
-	# 	output_file_path_chat_level = "../feature_engine/output/csopII_output_chat_level.csv",
-	# 	output_file_path_conv_level = "../feature_engine/output/csopII_output_conversation_level.csv"
-	# )
+	csopII_feature_builder = FeatureBuilder(
+		input_file_path = "../feature_engine/data/raw_data/csopII_conversations_withblanks.csv",
+		output_file_path_chat_level = "../feature_engine/output/csopII_output_chat_level.csv",
+		output_file_path_conv_level = "../feature_engine/output/csopII_output_conversation_level.csv"
+	)
 
-	# csopII_feature_builder.featurize(col="message")
+	csopII_feature_builder.featurize(col="message")
 
 	# DAT - Divergent Association Task
 	# dat_feature_builder = FeatureBuilder(
