a47006e7b59cb7152672b9af99092054e4fdc2f5
Shruti Agarwal <agshruti@Shrutis-MBP.fios-router.home>
2023-06-15 19:48:58 -0400

    finishing implementation of four discursive features #122

---
+++ feature_engine/utils/assign_chunk_nums.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def reduce_chunks(num_rows, max_num_chunks):
+    if (num_rows < max_num_chunks * 2):
+        max_num_chunks = int(num_rows / 2)
+    if max_num_chunks < 1:
+        return 1
+    else:
+        return max_num_chunks
+    
+
+# Assign chunk numbers to the chats within each conversation
+def assign_chunk_nums(chat_data, num_chunks):
+
+    # Calculate the total number of rows per conversation
+    conversation_lengths = chat_data.groupby('conversation_num').size()
+    # num_conversations = chat_data['conversation_num'].nunique()
+
+    chunks = conversation_lengths.apply(lambda x: reduce_chunks(x, num_chunks))
+
+    # Calculate the chunk size based on the total number of conversations
+    chunk_size = np.ceil(conversation_lengths / (chunks + 1)) # I found that using 10 in the bottom actually only generates 9 groups, so here I use 11 ... (weird)
+    
+    for i, group in chat_data.groupby('conversation_num'): # for each group
+        chunk_num = 0
+        counter = 0
+
+        for chat_id in group.index.values: # iterate over the index values
+            chat_data.at[chat_id, 'chunk_num'] = str(chunk_num)
+
+            counter += 1
+
+            #if counter = 1 for the last row of a group (implies last chunk has one element), and the chunk num > 0, then just make the last one - 1
+            if counter == 1 and chunk_num > 0 and chat_id == group.index.values[-1]:
+                chat_data.at[chat_id, 'chunk_num'] = str(chunk_num - 1)
+
+            if counter == chunk_size[i] and chunk_num < chunks[i] - 1: # assign any extras to the last chunk
+                chunk_num += 1
+                counter = 0    
+
+    return(chat_data)
