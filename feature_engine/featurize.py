d8c7311464bd17f2e13ba15f2e725c8ee14465dd
Shruti Agarwal <agshruti@Shrutis-MBP.fios-router.home>
2023-06-15 20:13:54 -0400

    commenting out csop

--- feature_engine/featurize.py
+++ feature_engine/featurize.py
@@ -46,31 +46,31 @@ if __name__ == "__main__":
 	# # FULL DATASETS BELOW
 
 	# Juries
-	# jury_feature_builder = FeatureBuilder(
-	# 	input_file_path = "../feature_engine/data/raw_data/jury_conversations_with_outcome_var.csv",
-	# 	output_file_path_chat_level = "../feature_engine/output/jury_output_chat_level.csv",
-	# 	output_file_path_conv_level = "../feature_engine/output/jury_output_conversation_level.csv"
-	# )
+	jury_feature_builder = FeatureBuilder(
+		input_file_path = "../feature_engine/data/raw_data/jury_conversations_with_outcome_var.csv",
+		output_file_path_chat_level = "../feature_engine/output/jury_output_chat_level.csv",
+		output_file_path_conv_level = "../feature_engine/output/jury_output_conversation_level.csv"
+	)
 
-	# jury_feature_builder.featurize(col="message")
+	jury_feature_builder.featurize(col="message")
 
 	# CSOP (Abdullah)
-	csop_feature_builder = FeatureBuilder(
-		input_file_path = "../feature_engine/data/raw_data/csop_conversations_withblanks.csv",
-		output_file_path_chat_level = "../feature_engine/output/csop_output_chat_level.csv",
-		output_file_path_conv_level = "../feature_engine/output/csop_output_conversation_level.csv"
-	)
+	# csop_feature_builder = FeatureBuilder(
+	# 	input_file_path = "../feature_engine/data/raw_data/csop_conversations_withblanks.csv",
+	# 	output_file_path_chat_level = "../feature_engine/output/csop_output_chat_level.csv",
+	# 	output_file_path_conv_level = "../feature_engine/output/csop_output_conversation_level.csv"
+	# )
 
-	csop_feature_builder.featurize(col="message")
+	# csop_feature_builder.featurize(col="message")
 
-	# CSOP II (Nak Won Rim)
-	csopII_feature_builder = FeatureBuilder(
-		input_file_path = "../feature_engine/data/raw_data/csopII_conversations_withblanks.csv",
-		output_file_path_chat_level = "../feature_engine/output/csopII_output_chat_level.csv",
-		output_file_path_conv_level = "../feature_engine/output/csopII_output_conversation_level.csv"
-	)
+	# # CSOP II (Nak Won Rim)
+	# csopII_feature_builder = FeatureBuilder(
+	# 	input_file_path = "../feature_engine/data/raw_data/csopII_conversations_withblanks.csv",
+	# 	output_file_path_chat_level = "../feature_engine/output/csopII_output_chat_level.csv",
+	# 	output_file_path_conv_level = "../feature_engine/output/csopII_output_conversation_level.csv"
+	# )
 
-	csopII_feature_builder.featurize(col="message")
+	# csopII_feature_builder.featurize(col="message")
 
 	# DAT - Divergent Association Task
 	# dat_feature_builder = FeatureBuilder(
