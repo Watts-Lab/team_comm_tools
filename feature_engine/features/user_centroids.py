ba5cd0007f01dd112ccc39e5dd6b368d639e68dc
Shruti Agarwal <agshruti@Shrutis-MBP.fios-router.home>
2023-07-20 13:42:54 -0400

    implement user infrastructure #113

--- feature_engine/features/user_centroids.py
+++ feature_engine/features/user_centroids.py
@@ -1,4 +1,4 @@
-from get_all_DD_features import *
+from .get_all_DD_features import *
 import pandas as pd
 import numpy as np
 
@@ -9,7 +9,7 @@ def get_user_centroids(chat_data, vect_data):
     # Format data
     chats['message_embedding'] = conv_to_float_arr(vect_data['message_embedding'].to_frame())
 
-    user_centroid_per_conv = pd.DataFrame(chat_data.groupby(['conversation_num','speaker_nickname'])['message_embedding'].apply(np.mean)).reset_index().rename(columns={'message_embedding':'mean_embedding'})
+    user_centroid_per_conv = pd.DataFrame(chats.groupby(['conversation_num','speaker_nickname'])['message_embedding'].apply(np.mean)).reset_index().rename(columns={'message_embedding':'mean_embedding'})
 
     return user_centroid_per_conv['mean_embedding']
 
