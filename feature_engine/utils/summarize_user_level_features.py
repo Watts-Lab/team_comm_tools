4a0507c63ab64d01ba5246bd4da5f9e2a3a268d1
Shruti Agarwal <agshruti@Shrutis-MBP.fios-router.home>
2023-07-29 19:01:27 -0400

    user + chat level aggregates #113

--- feature_engine/utils/summarize_user_level_features.py
+++ feature_engine/utils/summarize_user_level_features.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 '''
+These functions are for the purpose of calculating aggregates of various user level features at the conversation level.
+
+'''
+
+'''
 function: get_count_dataframe
 
 Returns a dataframe that gets the total number of X per individual, where X could be # words, etc.
@@ -8,13 +13,13 @@ Returns a dataframe that gets the total number of X per individual, where X coul
 @param chat_level_data = a dataframe in which each row is one chat.
 @param on_column = the name of the numeric column, X, which is summed per individual
 '''
-# def get_count_dataframe(chat_level_data, on_column, speaker_id = "speaker_nickname"):
-# 	grouped_conversation_data = chat_level_data[["conversation_num", speaker_id, on_column]].groupby(["conversation_num", speaker_id]).sum().reset_index() 
-#     # gets this dataframe:
-# 	# Batch# Round# Speaker  Total Number of Words
-# 	# 0 	 1      Priya    100
-# 	# 0      1      Yuluan   90
-# 	return(grouped_conversation_data)
+def get_count_dataframe(chat_level_data, on_column, speaker_id = "speaker_nickname"):
+	grouped_conversation_data = chat_level_data[["conversation_num", speaker_id, on_column]].groupby(["conversation_num", speaker_id]).sum().reset_index() 
+    # gets this dataframe:
+	# Batch# Round# Speaker  Total Number of Words
+	# 0 	 1      Priya    100
+	# 0      1      Yuluan   90
+	return(grouped_conversation_data)
 
 '''
 function: get_average()
@@ -25,7 +30,7 @@ Returns: grouped dataframe; [conversation_num, average_of_input_column]
 @param column_to_summarize = the column being aggregated
 @param new_column_name = the desired name of the new summary column
 '''
-def get_average(user_level_data, column_to_summarize, new_column_name):
+def get_average_user(user_level_data, column_to_summarize, new_column_name):
 	# grouped_conversation_data = get_count_dataframe(chat_level_data, column_to_summarize)
 	user_level_data[new_column_name] = user_level_data.groupby(["conversation_num"], sort=False)[column_to_summarize].transform(lambda x: np.mean(x))
 	return(user_level_data[["conversation_num", new_column_name]].drop_duplicates())
@@ -39,7 +44,7 @@ Returns: grouped dataframe; [conversation_num, max_of_input_column]
 @param column_to_summarize = the column being aggregated
 @param new_column_name
 '''
-def get_max(user_level_data, column_to_summarize, new_column_name):
+def get_max_user(user_level_data, column_to_summarize, new_column_name):
 	# grouped_conversation_data = get_count_dataframe(chat_level_data, column_to_summarize)
 	user_level_data[new_column_name] = user_level_data.groupby(["conversation_num"], sort=False)[column_to_summarize].transform(max)
 	return(user_level_data[["conversation_num", new_column_name]].drop_duplicates())
@@ -53,7 +58,7 @@ Returns: grouped dataframe;[conversation_num, min_of_input_column]
 @param column_to_summarize = the column being aggregated
 @param new_column_name
 '''
-def get_min(user_level_data, column_to_summarize, new_column_name):
+def get_min_user(user_level_data, column_to_summarize, new_column_name):
 	# grouped_conversation_data = get_count_dataframe(chat_level_data, column_to_summarize)
 	user_level_data[new_column_name] = user_level_data.groupby(["conversation_num"], sort=False)[column_to_summarize].transform(min)
 	return(user_level_data[["conversation_num", new_column_name]].drop_duplicates())
@@ -67,7 +72,7 @@ Returns: grouped dataframe; [conversation_num, stdev_of_input_column]
 @param column_to_summarize = the column being aggregated
 @param new_column_name
 '''
-def get_stdev(user_level_data, column_to_summarize, new_column_name):
+def get_stdev_user(user_level_data, column_to_summarize, new_column_name):
 	# grouped_conversation_data = get_count_dataframe(chat_level_data, column_to_summarize)
 	user_level_data[new_column_name] = user_level_data.groupby(["conversation_num"], sort=False)[column_to_summarize].transform(lambda x: np.std(x))
 	return(user_level_data[["conversation_num", new_column_name]].drop_duplicates())
@@ -81,7 +86,7 @@ Returns: grouped dataframe; [conversation_num, stdev_of_input_column]
 @param column_to_summarize = the column being aggregated
 @param new_column_name
 '''
-def get_sum(user_level_data, column_to_summarize, new_column_name):
+def get_sum_user(user_level_data, column_to_summarize, new_column_name):
 	# grouped_conversation_data = get_count_dataframe(chat_level_data, column_to_summarize)
 	user_level_data[new_column_name] = user_level_data.groupby(["conversation_num"], sort=False)[column_to_summarize].transform(lambda x: np.sum(x))
 	return(user_level_data[["conversation_num", new_column_name]].drop_duplicates())
