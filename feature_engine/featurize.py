7b250cde056cabe07eeceb6007ccbfb51da509c0
Shruti Agarwal <agshruti@lev-04820.apn.wlan.private.upenn.edu>
2023-09-30 17:35:24 -0400

    output pipeline refined and turns toggle functionality complete

--- feature_engine/featurize.py
+++ feature_engine/featurize.py
@@ -21,10 +21,11 @@ if __name__ == "__main__":
 	# Tiny Juries
 	feature_builder = FeatureBuilder(
 		input_file_path = "../feature_engine/data/raw_data/juries_tiny_for_testing.csv",
-		output_file_path_chat_level = "../feature_engine/output/jury_TINY_output_chat_level.csv",
-		output_file_path_user_level = "../feature_engine/output/jury_TINY_output_user_level.csv",
-		output_file_path_conv_level = "../feature_engine/output/jury_TINY_output_conversation_level.csv",
-		turns = False
+		output_file_path_chat_level = "../feature_engine/output/chat/jury_TINY_output_chat_level.csv",
+		output_file_path_turn_level = "../feature_engine/output/turn/jury_TINY_output_turn_level.csv",
+		output_file_path_user_level = "../feature_engine/output/user/jury_TINY_output_user_level.csv",
+		output_file_path_conv_level = "../feature_engine/output/conv/jury_TINY_output_conversation_level.csv",
+		turns = True
 	)
 	feature_builder.featurize(col="message")
 
