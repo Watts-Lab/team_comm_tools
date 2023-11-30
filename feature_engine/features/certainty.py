ded9f8015fc3775fa52d98cbf78a9379fcb94b10
Xinlan Emily Hu <xehu@stanford.edu>
2023-11-29 19:24:39 -0500

    change default certainty value + use the punctuated version of chat

--- feature_engine/features/certainty.py
+++ feature_engine/features/certainty.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import re
 
+# Note: This feature requires the message WITH punctuation.
+
 # parse certainty lexicon, compile into master regex, delimited by | 
 certainty = pd.read_csv("./features/lexicons/certainty.txt").sort_values(["NumWords", "NumCharacters"], ascending=False)
 master_regex = certainty["Word"].str.cat(sep='\\b|') + "\\b"
@@ -8,6 +10,9 @@ master_regex = certainty["Word"].str.cat(sep='\\b|') + "\\b"
 
 def get_certainty(chat): 
     
+    # default certainty value is 4.5; aka a "neutral" statement in the event we don't find anything
+    DEFAULT_CERTAINTY = 4.5
+
     # pattern match via re library
     certainty_score = 0
     matches = re.findall(master_regex, chat)
@@ -17,5 +22,5 @@ def get_certainty(chat):
 
     # safeguard against division by zero error
     if (len(matches) == 0):
-        return certainty_score
+        return DEFAULT_CERTAINTY 
     return (certainty_score / len(matches))
