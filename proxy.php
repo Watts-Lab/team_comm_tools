b54cd08e9c1888ae6212efafe1b27046f4f09887
Xinlan Emily Hu <xehu@stanford.edu>
2023-12-18 15:53:36 -0500

    update proxy

--- proxy.php
+++ proxy.php
@@ -1,4 +1,22 @@
 <?php
 $url = $_GET['url'];
-echo file_get_contents($url);
+
+$options = [
+    'http' => [
+        'follow_location' => 1,
+        'max_redirects' => 10,
+    ],
+];
+
+$context = stream_context_create($options);
+
+$content = file_get_contents($url, false, $context);
+
+if ($content === false) {
+    http_response_code(500);
+    echo "Failed to fetch content.";
+} else {
+    header('Content-Type: text/html');
+    echo $content;
+}
 ?>
