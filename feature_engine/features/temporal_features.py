f48666103aeb6476867857631f6eede94dd7de7d
Xinlan Emily Hu <xehu@wharton.upenn.edu>
2024-02-13 15:03:43 -0500

    catch errors in time related features

--- feature_engine/features/temporal_features.py
+++ feature_engine/features/temporal_features.py
@@ -6,17 +6,23 @@ def get_time_diff(df,on_column):
     df[on_column] = df[on_column].replace('NULL_TIME', None)
 
     #convert timestamp column to datetime type (in minutes)
-    if(isinstance(df[on_column][0], str)): # String datetime, e.g., '2023-02-20 09:00:00'
-        df[on_column] = pd.to_datetime(df[on_column])
-    elif(isinstance(df[on_column][0], np.int64)): # Int Unix dateime, e.g., '1677262112288'
-        df[on_column] = pd.to_datetime(df[on_column], unit='ms')
+    try:
+        if(isinstance(df[on_column][0], str)): # String datetime, e.g., '2023-02-20 09:00:00'
+            df[on_column] = pd.to_datetime(df[on_column])
+        elif(isinstance(df[on_column][0], np.int64)): 
+            df[on_column] = pd.to_datetime(df[on_column], unit='ms')
+        
+        # set and zero time_diff column
+        df["time_diff"] = np.zeros(len(df))
 
-    # set and zero time_diff column
-    df["time_diff"] = np.zeros(len(df))
-
-    for i in range(1, len(df)):
-        if df.loc[i, "conversation_num"] == df.loc[i-1, "conversation_num"]: # only do this if they're in the same conversation
-            df.loc[i, "time_diff"] = (df.loc[i, on_column] - df.loc[(i-1), on_column]) / pd.Timedelta(seconds=1)
+        for i in range(1, len(df)):
+            if df.loc[i, "conversation_num"] == df.loc[i-1, "conversation_num"]: # only do this if they're in the same conversation
+                df.loc[i, "time_diff"] = (df.loc[i, on_column] - df.loc[(i-1), on_column]) / pd.Timedelta(seconds=1)
+    except TypeError:
+        # dateTime conversion failed, which means that we can likely treat it as just an int representing # seconds elapsed
+        for i in range(1, len(df)):
+            if df.loc[i, "conversation_num"] == df.loc[i-1, "conversation_num"]: # only do this if they're in the same conversation
+                df.loc[i, "time_diff"] = (df.loc[i, on_column] - df.loc[(i-1), on_column])
 
     return df['time_diff']
 
