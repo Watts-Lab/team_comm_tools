415c88cf64a2a0746bdd9fa005323ebf1c34b31f
Shruti Agarwal <agshruti@lev-07551.apn.wlan.private.upenn.edu>
2024-03-13 21:17:41 -0400

    removing entire certainty

--- feature_engine/features/certainty.py
+++ feature_engine/features/certainty.py
@@ -7,24 +7,26 @@ from pathlib import Path
 
 # parse certainty lexicon, compile into master regex, delimited by | 
 # Construct the absolute path to certainty.txt using the current script directory
-current_script_directory = Path(__file__).resolve().parent
-certainty_file_path = current_script_directory / "lexicons/certainty.txt"
-certainty = pd.read_csv(certainty_file_path).sort_values(["NumWords", "NumCharacters"], ascending=False)
-master_regex = certainty["Word"].str.cat(sep='\\b|') + "\\b"
+# current_script_directory = Path(__file__).resolve().parent
+# certainty_file_path = current_script_directory / "lexicons/certainty.txt"
+# certainty = pd.read_csv(certainty_file_path).sort_values(["NumWords", "NumCharacters"], ascending=False)
+# master_regex = certainty["Word"].str.cat(sep='\\b|') + "\\b"
 
 def get_certainty(chat): 
+
+    return 0
     
     # default certainty value is 4.5; aka a "neutral" statement in the event we don't find anything
-    DEFAULT_CERTAINTY = 4.5
+    # DEFAULT_CERTAINTY = 4.5
 
-    # pattern match via re library
-    certainty_score = 0
-    matches = re.findall(master_regex, chat)
+    # # pattern match via re library
+    # certainty_score = 0
+    # matches = re.findall(master_regex, chat)
 
-    for match in matches:
-        certainty_score += certainty.loc[certainty['Word'] == match]["Certainty"].iloc[0]
+    # for match in matches:
+    #     certainty_score += certainty.loc[certainty['Word'] == match]["Certainty"].iloc[0]
 
-    # safeguard against division by zero error
-    if (len(matches) == 0):
-        return DEFAULT_CERTAINTY 
-    return (certainty_score / len(matches))
+    # # safeguard against division by zero error
+    # if (len(matches) == 0):
+    #     return DEFAULT_CERTAINTY 
+    # return (certainty_score / len(matches))
