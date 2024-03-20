6a1e1e3cc14d985f7b8ec6870a34f57ec5ba0709
Shruti Agarwal <agshruti@lev-16113.apn.wlan.private.upenn.edu>
2024-03-20 19:58:31 -0400

    verbose error handling pytest

--- feature_engine/testing/test_feature_metrics.py
+++ feature_engine/testing/test_feature_metrics.py
@@ -2,12 +2,34 @@ import pytest
 import pandas as pd
 import numpy as np
 from numpy import nan
+import logging
 
-test_df =  pd.read_csv("../output/chat/test_num_words.csv")
+test_df =  pd.read_csv("../output/chat/reddit_test_chat_level.csv")
 # test_df['test_pass'] = test_df.apply(lambda row: row[row['expected_column']] == row['expected_value'], axis=1)
 # test_df['obtained_value'] = test_df.apply(lambda row: row[row['expected_column']], axis=1)
 # test_df[["message", "expected_column", "expected_value", "obtained_value", "test_pass"]]
 
+# Set up logging to capture only error messages
+logger = logging.getLogger("pytest_prints")
+logger.setLevel(logging.ERROR)
+
+# Add a console handler to the logger
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.ERROR)
+logger.addHandler(console_handler)
+
 @pytest.mark.parametrize("row", test_df.iterrows())
 def test_unit_equality(row):
-    assert row[1][row[1]['expected_column']] == row[1]['expected_value']
+    actual = row[1][row[1]['expected_column']]
+    expected = row[1]['expected_value']
+    
+    try:
+        assert actual == expected
+    except AssertionError:
+        logger.error("")
+        logger.error("------TEST FAILED------")
+        logger.error("Testing %s for message: %s ", row[1]['expected_column'], row[1]['message_original'])
+        logger.error("Expected value: %s ", expected)
+        logger.error("Actual value: %s", actual)
+
+        raise  # Re-raise the AssertionError to mark the test as failed
