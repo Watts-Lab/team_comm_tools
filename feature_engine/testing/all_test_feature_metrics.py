edc25f55285b8344c1b209789ae0620a44828be7
Shruti Agarwal <agshruti@lev-16113.apn.wlan.private.upenn.edu>
2024-03-20 20:44:49 -0400

    update workflow

--- feature_engine/testing/all_test_feature_metrics.py
+++ feature_engine/testing/all_test_feature_metrics.py
@@ -4,7 +4,12 @@ import numpy as np
 from numpy import nan
 import logging
 
+test_input_df = pd.read_csv("../feature_engine/reddit_test_chat_level.csv")
 test_chat_df =  pd.read_csv("../output/chat/reddit_test_chat_level.csv")
+test_conv_df_output =  pd.read_csv("../output/conv/reddit_test_chat_level.csv")
+# join test_input_df with test_conv_df_output on conversation_num id
+test_conv_df = pd.merge(test_input_df, test_conv_df_output, on='conversation_num')
+# test_conv_df = pd.read_csv("test_turn_taking.csv")
 
 # test_df['test_pass'] = test_df.apply(lambda row: row[row['expected_column']] == row['expected_value'], axis=1)
 # test_df['obtained_value'] = test_df.apply(lambda row: row[row['expected_column']], axis=1)
@@ -34,3 +39,30 @@ def test_chat_unit_equality(row):
         logger.error("Actual value: %s", actual)
 
         raise  # Re-raise the AssertionError to mark the test as failed
+
+
+@pytest.mark.parametrize("conversation_num, conversation_rows", test_conv_df.groupby('conversation_num'))
+def test_conv_unit_equality(conversation_num, conversation_rows):
+    test_failed = False
+    expected_out = ""
+    actual_out = ""
+
+    for _, row in conversation_rows.iterrows():
+        actual = row[row['expected_column']]
+        expected = row['expected_value']
+    
+    try:
+        assert actual == expected
+    except AssertionError:
+        expected_out = expected
+        actual_out = actual
+        test_failed = True
+
+    if test_failed:
+        logger.error("")
+        logger.error("------TEST FAILED------")
+        logger.error("Testing %s for conversation_num: %s ", row['expected_column'], conversation_num)
+        logger.error("Expected value: %s ", expected_out)
+        logger.error("Actual value: %s", actual_out)
+
+        raise
