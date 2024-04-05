cff9529df8e7936e161fdd06efb4b35fe55198e6
Xinlan Emily Hu <xehu@wharton.upenn.edu>
2024-04-04 22:46:54 -0400

    catch a div 0 error in turn-taking

--- feature_engine/features/turn_taking_features.py
+++ feature_engine/features/turn_taking_features.py
@@ -50,53 +50,11 @@ Returns the turn-taking index for each speaker
 """
 
 def count_turn_taking_index(input_data):
-    return (len(count_turns(input_data)) - 1) / (len(input_data) - 1)
+    if(len(input_data) == 1): # there is only 1 speaker for one row; catch a divide by zero error
+        return 0
+    else:
+        return (len(count_turns(input_data)) - 1) / (len(input_data) - 1)
 
 def get_turn(input_data):
     turn_calculated_2 = input_data.groupby("conversation_num").apply(lambda x : count_turn_taking_index(x)).reset_index().rename(columns={0: "turn_taking_index"})
     return turn_calculated_2
-
-
-
-# import unittest
-# class TestTurnTakingFeatures(unittest.TestCase):
-#
-#     def test_count_turns(self):
-#         input_data = pd.DataFrame({
-#             "conversation_num": ["ab", "ab", "ab", "ab", "ab"],
-#             "speaker_nickname": ["A", "A", "B", "A", "B"]
-#         })
-#
-#         expected_output = pd.DataFrame({
-#             "speaker_nickname": ["A", "B", "A", "B"],
-#             "turn_count": [2, 1, 1, 1]
-#         })
-#
-#         self.assertTrue(count_turns(input_data).equals(expected_output))
-#
-#     def test_count_turn_taking_index(self):
-#         input_data = pd.DataFrame({
-#             "conversation_num": ["ab", "ab", "ab", "ab", "ab"],
-#             "speaker_nickname": ["A", "A", "B", "A", "B"]
-#         })
-#
-#         expected_output = 0.75  # (4 - 1) / (5 - 1)
-#
-#         self.assertEqual(count_turn_taking_index(input_data), expected_output)
-#
-#     def test_get_turn(self):
-#         input_data = pd.DataFrame({
-#             "conversation_num": ["ab", "ab", "ab", "ab", "ab", "cd", "cd"],
-#             "speaker_nickname": ["A", "A", "B", "A", "B", "C", "C"]
-#         })
-#
-#         expected_output = pd.DataFrame({
-#             "conversation_num": ["ab", "cd"],
-#             "turn_taking_index": [0.75, 0]
-#         })
-#
-#         self.assertTrue(get_turn(input_data).equals(expected_output))
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
