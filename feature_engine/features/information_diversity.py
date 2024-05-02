a37959afd68763d18ff74d745bc463038b15de13
kumarnik1 <123492959+kumarnik1@users.noreply.github.com>
2024-05-02 00:08:02 -0400

    comments

--- feature_engine/features/information_diversity.py
+++ feature_engine/features/information_diversity.py
@@ -19,10 +19,17 @@ from gensim.utils import simple_preprocess
 from gensim.models.ldamodel import LdaModel
 
 def get_info_diversity(df):
+    """
+    Computes information diversity (value between 0 and 1 inclusive) for all conversations
+    """
     info_div_score = df.groupby("conversation_num").apply(lambda x : info_diversity(x)).reset_index().rename(columns={0: "info_diversity"})
     return info_div_score
 
 def info_diversity(df):
+    """
+    Preprocess data and then create numeric mapping of words in dataset to pass into LDA model
+    Uses n = 20 topics 
+    """
 
     processed_data = df['message'].apply(preprocessing).tolist()
 
@@ -33,16 +40,24 @@ def info_diversity(df):
 
     topics = [lda.get_document_topics(bow) for bow in full_corpus]
 
-    ID = calculate_ID_score(topics, 10)
+    ID = calculate_ID_score(topics, 20)
     return ID
 
 def preprocessing(data):
+        """
+        Preprocesses the data by lowercasing, lemmatizing, and removing words of size less than 4
+        """
         le=WordNetLemmatizer()
         word_tokens=word_tokenize(data.lower())
-        tokens=[le.lemmatize(w) for w in word_tokens if w not in stopword and len(w)>3]
+        tokens=[le.lemmatize(w) for w in word_tokens if w not in stopword and len(w) > 3]
         return tokens
 
 def calculate_ID_score(doc_topics, num_topics):
+        """
+        Computes info diversity score as suggested in Reidl & Woodley (2017); determine a topic vector 
+        for every message, then compute a mean topic vector across all messages, and measure the average 
+        cosine similarity between the message vectors and the mean vector
+        """
         topic_matrix = []
         for doc in doc_topics:
             topic_dist = np.zeros(num_topics)
