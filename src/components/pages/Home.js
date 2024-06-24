bdada1110732bf1048ef412139078bda4b7d1df3
amytangzheng <amy.tang.zheng@gmail.com>
2024-06-24 13:53:54 -0400

    deploying from gh-pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Home() {
+    return (
+        <h1 className='home'>
+            Welcome
+        </h1>
+    );
+}
+
+export default Home;
---
+++ src/components/pages/Research.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+
+function Research() {
+  return (
+    <div>
+      <h1 className='research'>
+        Research
+      </h1>
+    </div>
+  );
+}
+
+export default Research;
