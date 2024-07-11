22148e3d1baf8c13fe72cdcbe8c541329edc45b8
amytangzheng <amy.tang.zheng@gmail.com>
2024-07-11 15:01:20 -0400

    updated homepage

--- website/src/App.js
+++ website/src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/team-process-map" element={<Home />} />
           <Route path="/Research" element={<Research />} />
           <Route path="/Team" element={<Team />} />
           <Route path="/HowItWorks" element={<HowItWorks />} />
