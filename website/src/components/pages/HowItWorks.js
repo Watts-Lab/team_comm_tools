78b3fae3333e9c83b1f1f8a62dfdd1183677e906
amytangzheng <amy.tang.zheng@gmail.com>
2024-07-25 17:14:40 -0400

    feature table

--- website/src/components/pages/HowItWorks.js
+++ website/src/components/pages/HowItWorks.js
@@ -1,5 +1,36 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import '../../App.css';
+
+const feature_dict = {
+  "Named Entity Recognition": {
+    "columns": ["num_named_entity", "named_entities"],
+    "file": "./features/named_entity_recognition_features.py",
+    "level": "Chat",
+    "semantic_grouping": "Content",
+    "description": "This feature detects whether a user is talking about (or to) someone else in a conversation.",
+    "references": "N/A",
+    "wiki_link": "https://conversational-featurizer.readthedocs.io/en/latest/features_conceptual/named_entity_recognition.html",
+    "function": 'ChatLevelFeaturesCalculator.get_named_entity',
+    "dependencies": [],
+    "preprocess": [],
+    "vect_data": 'False',
+    "bert_sentiment_data": 'False'
+  },
+  "Positivity (BERT)": {
+    "columns": ["positive_bert", "negative_bert", "neutral_bert"],
+    "file": "./utils/check_embeddings.py",
+    "level": "Chat",
+    "semantic_grouping": "Emotion",
+    "description": "The extent to which a statement is positive, negative, or neutral, as assigned by Cardiffnlp/twitter-roberta-base-sentiment-latest. The total scores (Positive, Negative, Neutral) sum to 1.",
+    "references": "(Hugging Face, 2023)",
+    "wiki_link": "https://github.com/Watts-Lab/team-process-map/wiki/BERT-Sentiment-Analysis-Feature",
+    "function": 'ChatLevelFeaturesCalculator.concat_bert_features',
+    "dependencies": [],
+    "preprocess": [],
+    "vect_data": 'False',
+    "bert_sentiment_data": 'True'
+  }
+}
 
 function HowItWorks() {
   useEffect(() => {
@@ -55,6 +86,38 @@ function HowItWorks() {
         <p>
           We encourage anyone interested in developing a feature to either contact <a href="http://xinlanemilyhu.com">Xinlan Emily Hu</a> or to make a pull request.
         </p>
+      </div>
+
+      <h1 className='how-it-works-headers'> Features </h1>
+      <div className='features'>
+        <table>
+          <thead>
+            <tr>
+              <th>Feature</th>
+              <th>Description</th>
+              <th>Columns</th>
+              <th>File</th>
+              <th>Level</th>
+              <th>Semantic Grouping</th>
+              <th>References</th>
+              <th>Wiki Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(feature_dict).map(([key, value]) => (
+              <tr key={key}>
+                <td className='feature-name'>{key}</td>
+                <td className='desc'>{value.description}</td>
+                <td className='norm'>{value.columns.join(', ')}</td>
+                <td className='norm'>{value.file}</td>
+                <td className='norm'>{value.level}</td>
+                <td className='norm'>{value.semantic_grouping}</td>
+                <td className='norm'>{value.references}</td>
+                <td className='norm'><a href={value.wiki_link} target="_blank" rel="noopener noreferrer">{value.wiki_link}</a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
     </div>
