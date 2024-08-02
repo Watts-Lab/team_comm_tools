48a29013882c155ddd5a239e831fd54cd738e211
Xinlan Emily Hu <xehu@wharton.upenn.edu>
2024-08-02 16:15:32 -0400

    commit setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Step 1: Install required packages
+# Step 1: Install the spaCy model
+echo "Installing spaCy model..."
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.1/en_core_web_sm-3.7.1.tar.gz --no-deps
+if [ $? -ne 0 ]; then
+    echo "Failed to install spaCy model."
+    exit 1
+fi
+
+# Step 2: Install required packages
 echo "Installing required packages from requirements.txt..."
 pip install -r requirements.txt
 if [ $? -ne 0 ]; then
@@ -8,23 +16,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Step 2: Install the spaCy model
-echo "Installing SpaCy model..."
-python -m spacy download en_core_web_sm
-if [ $? -ne 0 ]; then
-    echo "Failed to install spaCy model."
-    exit 1
-fi
-
 # Step 3: Run the import_nltk.py script
 echo "Running import_nltk.py..."
-python -c "
-import nltk
-nltk.download('nps_chat')
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-"
+python src/team_comm_tools/utils/import_nltk.py
 if [ $? -ne 0 ]; then
     echo "Failed to run import_nltk.py."
     exit 1
