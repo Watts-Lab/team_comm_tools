dc78b097b76c77ad493c7dca2855ca61b8ad2f94
Xinlan Emily Hu <xehu@cs.stanford.edu>
2024-08-15 17:38:57 -0400

    Urgent fix to remove LIWC lexicons from public repo (#279) (#280)
    * delete small test lexicons
    
    * move .pkl files to assets and remove from GH
    
    * filesystem cleanup
    
    * update certainty pickle location
    
    * remove unpickling certainty
    
    * remove lexicons from pyproject
    
    * change lexical pkl path
    
    * add error handling when lexicons are not found
    
    * update warning message
    
    * add legal caveat and update name of certainty pkl to be correct

--- src/team_comm_tools/features/lexical_features_v2.py
+++ src/team_comm_tools/features/lexical_features_v2.py
@@ -42,17 +42,20 @@ def liwc_features(chat_df: pd.DataFrame, message_col) -> pd.DataFrame:
 		pd.DataFrame: Dataframe of the lexical features stacked as columns.
 	"""
 	# Load the preprocessed lexical regular expressions
-	current_dir = os.path.dirname(__file__)
-	lexicon_pkl_file_path = os.path.join(current_dir, './assets/lexicons_dict.pkl')
-	lexicon_pkl_file_path = os.path.abspath(lexicon_pkl_file_path)
-	with open(lexicon_pkl_file_path, "rb") as lexicons_pickle_file:
-		lexicons_dict = pickle.load(lexicons_pickle_file)
-	
-	# Return the lexical features stacked as columns
-	return pd.concat(
-		# Finding the # of occurrences of lexicons of each type for all the messages.
-		[pd.DataFrame(chat_df[message_col + "_original"].apply(lambda chat: get_liwc_rate(regex, chat)))\
-										.rename({message_col + "_original": lexicon_type + "_lexical_per_100"}, axis=1)\
-			for lexicon_type, regex in lexicons_dict.items()], 
-		axis=1
-	)
+	try:
+		current_dir = os.path.dirname(__file__)
+		lexicon_pkl_file_path = os.path.join(current_dir, './assets/lexicons_dict.pkl')
+		lexicon_pkl_file_path = os.path.abspath(lexicon_pkl_file_path)
+		with open(lexicon_pkl_file_path, "rb") as lexicons_pickle_file:
+			lexicons_dict = pickle.load(lexicons_pickle_file)
+		
+		# Return the lexical features stacked as columns
+		return pd.concat(
+			# Finding the # of occurrences of lexicons of each type for all the messages.
+			[pd.DataFrame(chat_df[message_col + "_original"].apply(lambda chat: get_liwc_rate(regex, chat)))\
+											.rename({message_col + "_original": lexicon_type + "_lexical_per_100"}, axis=1)\
+				for lexicon_type, regex in lexicons_dict.items()], 
+			axis=1
+		)
+	except:
+		print("WARNING: Lexicons not found. Skipping feature...")
