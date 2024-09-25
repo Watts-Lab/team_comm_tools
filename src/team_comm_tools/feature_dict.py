4030a113e0c434ac36cd2ac6693c4dfa3194983c
amytangzheng <amy.tang.zheng@gmail.com>
2024-09-25 16:07:29 -0400

    testing github-actions feature_dict

--- src/team_comm_tools/feature_dict.py
+++ src/team_comm_tools/feature_dict.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 feature_dict = { # TODO: customize preprocess methods
     # Chat Level
-    "Named Entity": {
+    "Named Entity Recognition": {
     "columns": ["num_named_entity", "named_entities"],
     "file": "./features/named_entity_recognition_features.py",
     "level": "Chat",
