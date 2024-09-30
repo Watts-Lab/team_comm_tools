b9147bb75abd58609c87b9aab91953cb92292406
Xinlan Emily Hu <xehu@cs.stanford.edu>
2024-09-30 19:38:36 -0400

    Update run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -31,71 +31,71 @@ if __name__ == "__main__":
 		
 	# TESTING DATASETS -------------------------------
 
-	# testing_chat = FeatureBuilder(
-	# 	input_df = chat_df,
-	# 	vector_directory = "./vector_data/",
-	# 	output_file_path_chat_level = "./output/chat/test_chat_level_chat.csv",
-	# 	output_file_path_user_level = "./output/user/test_chat_level_user.csv",
-	# 	output_file_path_conv_level = "./output/conv/test_chat_level_conv.csv",
-	# 	custom_features = [ # these require vect_data, so they now need to be explicitly included in order to calculate them
-    #         "(BERT) Mimicry",
-    #         "Moving Mimicry",
-    #         "Forward Flow",
-    #         "Discursive Diversity"
-    #     ],
-	# 	turns = False,
-	# )
-	# testing_chat.featurize(col="message")
+	testing_chat = FeatureBuilder(
+	 	input_df = chat_df,
+	 	vector_directory = "./vector_data/",
+	 	output_file_path_chat_level = "./output/chat/test_chat_level_chat.csv",
+	 	output_file_path_user_level = "./output/user/test_chat_level_user.csv",
+	 	output_file_path_conv_level = "./output/conv/test_chat_level_conv.csv",
+	 	custom_features = [ # these require vect_data, so they now need to be explicitly included in order to calculate them
+    	         "(BERT) Mimicry",
+    	         "Moving Mimicry",
+    	         "Forward Flow",
+    	         "Discursive Diversity"
+    	     ],
+	 	turns = False,
+	)
+	testing_chat.featurize()
 
-	# testing_conv = FeatureBuilder(
-	# 	input_df = conv_df,
-	# 	vector_directory = "./vector_data/",
-	# 	output_file_path_chat_level = "./output/chat/test_conv_level_chat.csv",
-	# 	output_file_path_user_level = "./output/user/test_conv_level_user.csv",
-	# 	output_file_path_conv_level = "./output/conv/test_conv_level_conv.csv",
-	# 	custom_features = [
-    #         "(BERT) Mimicry",
-    #         "Moving Mimicry",
-    #         "Forward Flow",
-    #         "Discursive Diversity"
-    #     ],
-	# 	turns = False,
-	# )
-	# testing_conv.featurize(col="message")
+	testing_conv = FeatureBuilder(
+	 	input_df = conv_df,
+	 	vector_directory = "./vector_data/",
+	 	output_file_path_chat_level = "./output/chat/test_conv_level_chat.csv",
+	 	output_file_path_user_level = "./output/user/test_conv_level_user.csv",
+	 	output_file_path_conv_level = "./output/conv/test_conv_level_conv.csv",
+	 	custom_features = [
+    	         "(BERT) Mimicry",
+    	         "Moving Mimicry",
+    	         "Forward Flow",
+    	         "Discursive Diversity"
+         ],
+	 	turns = False,
+	)
+	testing_conv.featurize()
 
-	# test_ner_feature_builder = FeatureBuilder(
-	# 	input_df = test_ner_df,
-	# 	ner_training_df = test_ner_training_df,
-	# 	vector_directory = "./vector_data/",
-	# 	output_file_path_chat_level = "./output/chat/test_named_entity_chat_level.csv",
-	# 	output_file_path_user_level = "./output/user/test_named_entity_user_level.csv",
-	# 	output_file_path_conv_level = "./output/conv/test_named_entity_conversation_level.csv",
-	# 	custom_features = [
-    #         "(BERT) Mimicry",
-    #         "Moving Mimicry",
-    #         "Forward Flow",
-    #         "Discursive Diversity"
-    #     ],
-	# 	turns = False,
-	# )
-	# test_ner_feature_builder.featurize(col="message")
+	test_ner_feature_builder = FeatureBuilder(
+	 	input_df = test_ner_df,
+	 	ner_training_df = test_ner_training_df,
+	 	vector_directory = "./vector_data/",
+	 	output_file_path_chat_level = "./output/chat/test_named_entity_chat_level.csv",
+	 	output_file_path_user_level = "./output/user/test_named_entity_user_level.csv",
+	 	output_file_path_conv_level = "./output/conv/test_named_entity_conversation_level.csv",
+	 	custom_features = [
+		"(BERT) Mimicry",
+    	        "Moving Mimicry",
+	    	"Forward Flow",
+   		"Discursive Diversity"
+         ],
+ 		turns = False,
+	)
+	test_ner_feature_builder.featurize()
 
-	# # testing perturbed chat level features
-	# testing_chat_complex = FeatureBuilder(
-	# 	input_df = chat_complex_df,
-	# 	vector_directory = "./vector_data/",
-	# 	output_file_path_chat_level = "./output/chat/test_chat_level_chat_complex.csv",
-	# 	output_file_path_user_level = "./output/user/test_chat_level_user_complex.csv",
-	# 	output_file_path_conv_level = "./output/conv/test_chat_level_conv_complex.csv",
-	# 	custom_features = [
-    #         "(BERT) Mimicry",
-    #         "Moving Mimicry",
-    #         "Forward Flow",
-    #         "Discursive Diversity"
-    #     ],
-	# 	turns = False,
-	# )
-	# testing_chat_complex.featurize(col="message")
+	# testing perturbed chat level features
+	testing_chat_complex = FeatureBuilder(
+	 	input_df = chat_complex_df,
+	 	vector_directory = "./vector_data/",
+	 	output_file_path_chat_level = "./output/chat/test_chat_level_chat_complex.csv",
+	 	output_file_path_user_level = "./output/user/test_chat_level_user_complex.csv",
+	 	output_file_path_conv_level = "./output/conv/test_chat_level_conv_complex.csv",
+	 	custom_features = [
+             	"(BERT) Mimicry",
+             	"Moving Mimicry",
+             	"Forward Flow",
+             	"Discursive Diversity"
+         ],
+	 	turns = False,
+	 )
+	testing_chat_complex.featurize()
 
 	# testing conv features
 	testing_conv_complex = FeatureBuilder(
@@ -114,36 +114,36 @@ if __name__ == "__main__":
 	)
 	testing_conv_complex.featurize()
 
-	# testing_conv_complex_ts = FeatureBuilder(
-	# 	input_df = conv_complex_timestamps_df,
-	# 	vector_directory = "./vector_data/",
-	# 	output_file_path_chat_level = "./output/chat/test_conv_level_chat_complex_ts.csv",
-	# 	output_file_path_user_level = "./output/user/test_conv_level_user_complex_ts.csv",
-	# 	output_file_path_conv_level = "./output/conv/test_conv_level_conv_complex_ts.csv",
-	# 	custom_features = [
-    #         "(BERT) Mimicry",
-    #         "Moving Mimicry",
-    #         "Forward Flow",
-    #         "Discursive Diversity"
-    #     ],
-	# 	turns = False,
-	# )
-	# testing_conv_complex_ts.featurize(col="message")
+	testing_conv_complex_ts = FeatureBuilder(
+		input_df = conv_complex_timestamps_df,
+	 	vector_directory = "./vector_data/",
+	 	output_file_path_chat_level = "./output/chat/test_conv_level_chat_complex_ts.csv",
+	 	output_file_path_user_level = "./output/user/test_conv_level_user_complex_ts.csv",
+	 	output_file_path_conv_level = "./output/conv/test_conv_level_conv_complex_ts.csv",
+	 	custom_features = [
+             "(BERT) Mimicry",
+             "Moving Mimicry",
+             "Forward Flow",
+             "Discursive Diversity"
+         ],
+	 	turns = False,
+	)
+	testing_conv_complex_ts.featurize()
 
-	# # testing forward flow
-	# testing_forward_flow = FeatureBuilder(
-	# 	input_df = test_forward_flow_df,
-	# 	vector_directory = "./vector_data/",
-	# 	output_file_path_chat_level = "./output/chat/test_forward_flow_chat.csv",
-	# 	output_file_path_user_level = "./output/user/test_forward_flow_user.csv",
-	# 	output_file_path_conv_level = "./output/conv/test_forward_flow_conv.csv",
-	# 	custom_features = [
-	# 		"(BERT) Mimicry",
-	# 		"Moving Mimicry",
-	# 		"Forward Flow",
-	# 		"Discursive Diversity"
-	# 	],
-	# 	turns = False,
-	# )
+	# testing forward flow
+	testing_forward_flow = FeatureBuilder(
+		input_df = test_forward_flow_df,
+	 	vector_directory = "./vector_data/",
+	 	output_file_path_chat_level = "./output/chat/test_forward_flow_chat.csv",
+	 	output_file_path_user_level = "./output/user/test_forward_flow_user.csv",
+	 	output_file_path_conv_level = "./output/conv/test_forward_flow_conv.csv",
+	 	custom_features = [
+	 		"(BERT) Mimicry",
+	 		"Moving Mimicry",
+	 		"Forward Flow",
+	 		"Discursive Diversity"
+	 	],
+		turns = False,
+	)
 
-	# testing_forward_flow.featurize(col="message")
+	testing_forward_flow.featurize()
