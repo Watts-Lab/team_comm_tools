f7f93d12f2064a2f314d4050b47c0b046808bff2
Xinlan Emily Hu <xehu@wharton.upenn.edu>
2024-11-11 11:10:32 -0500

    revert changes to featurize.py

--- examples/featurize.py
+++ examples/featurize.py
@@ -37,53 +37,53 @@ if __name__ == "__main__":
 
 	"""
 
-	# # Tiny Juries
-	# tiny_juries_feature_builder = FeatureBuilder(
-	# 	input_df = tiny_juries_df,
-	# 	grouping_keys = ["batch_num", "round_num"],
-	# 	output_file_base = "jury_TINY_output", # Naming output files using the output_file_base parameter (recommended)
-	# 	turns = False,
-	# 	custom_features = [
-	# 		"(BERT) Mimicry",
-	# 		"Moving Mimicry",
-	# 		"Forward Flow",
-	# 		"Discursive Diversity"]
-	# )
-	# tiny_juries_feature_builder.featurize()
-
-	# # Tiny multi-task
-	# tiny_multi_task_feature_builder = FeatureBuilder(
-	# 	input_df = tiny_multi_task_df,
-	# 	conversation_id_col = "stageId",
-	# 	# alternatively, you can name each output file separately. NOTE, however, that we don't directly use this path;
-	# 	# we modify the path to place outputs within the `output/chat`, `output/conv`, and `output/user` folders.
-	# 	output_file_path_chat_level = "./multi_task_TINY_output_chat_level_stageId_cumulative.csv",
-	# 	output_file_path_user_level = "./multi_task_TINY_output_user_level_stageId_cumulative.csv",
-	# 	output_file_path_conv_level = "./multi_task_TINY_output_conversation_level_stageId_cumulative.csv",
-	# 	turns = False
-	# )
-	# tiny_multi_task_feature_builder.featurize()
-
-	# FULL DATASETS BELOW ------------------------------------- #
-	
-	# Juries
-	jury_feature_builder = FeatureBuilder(
-		input_df = juries_df,
+	# Tiny Juries
+	tiny_juries_feature_builder = FeatureBuilder(
+		input_df = tiny_juries_df,
 		grouping_keys = ["batch_num", "round_num"],
-		vector_directory = "./vector_data/",
-		output_file_path_chat_level = "./jury_output_chat_level.csv",
-		output_file_path_user_level = "./jury_output_user_level.csv",
-		output_file_path_conv_level = "./jury_output_conversation_level.csv",
-		turns = True,
+		output_file_base = "jury_TINY_output", # Naming output files using the output_file_base parameter (recommended)
+		turns = False,
 		custom_features = [
 			"(BERT) Mimicry",
 			"Moving Mimicry",
 			"Forward Flow",
 			"Discursive Diversity"]
 	)
-	jury_feature_builder.featurize()
+	tiny_juries_feature_builder.featurize()
 
-	# # CSOP (Abdullah)
+	# Tiny multi-task
+	tiny_multi_task_feature_builder = FeatureBuilder(
+		input_df = tiny_multi_task_df,
+		conversation_id_col = "stageId",
+		# alternatively, you can name each output file separately. NOTE, however, that we don't directly use this path;
+		# we modify the path to place outputs within the `output/chat`, `output/conv`, and `output/user` folders.
+		output_file_path_chat_level = "./multi_task_TINY_output_chat_level_stageId_cumulative.csv",
+		output_file_path_user_level = "./multi_task_TINY_output_user_level_stageId_cumulative.csv",
+		output_file_path_conv_level = "./multi_task_TINY_output_conversation_level_stageId_cumulative.csv",
+		turns = False
+	)
+	tiny_multi_task_feature_builder.featurize()
+
+	# FULL DATASETS BELOW ------------------------------------- #
+	
+	# Juries
+	# jury_feature_builder = FeatureBuilder(
+	# 	input_df = juries_df,
+	# 	grouping_keys = ["batch_num", "round_num"],
+	# 	vector_directory = "./vector_data/",
+	# 	output_file_path_chat_level = "./jury_output_chat_level.csv",
+	# 	output_file_path_user_level = "./jury_output_user_level.csv",
+	# 	output_file_path_conv_level = "./jury_output_conversation_level.csv",
+	# 	turns = True,
+	# 	custom_features = [
+	# 		"(BERT) Mimicry",
+	# 		"Moving Mimicry",
+	# 		"Forward Flow",
+	# 		"Discursive Diversity"]
+	# )
+	# jury_feature_builder.featurize()
+
+	# CSOP (Abdullah)
 	# csop_feature_builder = FeatureBuilder(
 	# 	input_df = csop_df,
 	# 	vector_directory = "./vector_data/",
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 	# csop_feature_builder.featurize()
 
 
-	# # CSOP II (Nak Won Rim)
+	# CSOP II (Nak Won Rim)
 	# csopII_feature_builder = FeatureBuilder(
 	# 	input_df = csopII_df,
 	# 	vector_directory = "./vector_data/",
